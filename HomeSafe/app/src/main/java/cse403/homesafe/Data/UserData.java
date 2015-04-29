027dc8d780fe4e870a2d2f6fe9f31892188d2074
Vivek Paramasivam <Vivek@macreg11821.dyn.cs.washington.edu>
2015-04-28 18:55:23 -0700

    Stubbed Classes added in proper directories. No implementations yet.

---
+++ HomeSafe/app/src/main/java/cse403/homesafe/Data/UserData.java
@@ -0,0 +1,7 @@
+package cse403.homesafe.Data;
+
+/**
+ * Created by Vivek on 4/28/15.
+ */
+public class UserData {
+}
---
+++ HomeSafe/app/src/main/java/cse403/homesafe/Messaging/Email.java
@@ -0,0 +1,7 @@
+package cse403.homesafe.Messaging;
+
+/**
+ * Created by Vivek on 4/28/15.
+ */
+public class Email implements Message {
+}
