f0c42a9299b47829ff4aecaf9daeaf2e35f84137
Alex Hernandez <jahrndez@uw.edu>
2015-05-01 19:10:27 -0700

    Unit tests and activity tests work in isolation. Added emulator component in .yml file and changed gradle command

---
+++ HomeSafe/app/src/main/java/UnitTests/MessengerTest.java
@@ -0,0 +1,23 @@
+package UnitTests;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test class for Messenger.java
+ */
+public class MessengerTest extends TestCase {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testSendNotifications() {
+        assertEquals(1, 0);
+    }
+}
+
