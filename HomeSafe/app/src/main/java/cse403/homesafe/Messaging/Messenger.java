dfa6db2adbc79c2375bfdd5a1ff00c69e72a2136
Alex Hernandez <jahrndez@uw.edu>
2015-05-03 15:08:37 -0700

    Initial implementation of Messenger.sendNotifications()

--- HomeSafe/app/src/main/java/cse403/homesafe/Messaging/Messenger.java
+++ HomeSafe/app/src/main/java/cse403/homesafe/Messaging/Messenger.java
@@ -1,5 +1,9 @@
 package cse403.homesafe.Messaging;
 
+import java.util.List;
+
+import cse403.homesafe.Data.Contact;
+import cse403.homesafe.Data.Contacts;
 import cse403.homesafe.Data.Location;  // May substitute with android.location.Location
 
 /**
@@ -12,11 +16,19 @@ public class Messenger {
     private static Email email = Email.getInstance();  // For sending email
 
     /**
-     * Sends notifications to the specified tier of Contacts
-     * @param tier      Tier of contacts to be notified
+     * Sends notifications to the specified tier of Contacts with the specified
+     * last known user Location.
+     * @param tier Tier of contacts to be notified
      * @param location  Last known user location
      */
-    public static void sendNotifications (int tier, Location location) {
-
+    public static void sendNotifications (Contacts.Tier tier, Location location) {
+        Contacts contacts = new Contacts();
+        List<Contact> contactsNotified = contacts.getContactsInTier(tier);
+        for (Contact c : contactsNotified) {
+            if (!c.getEmail().equals(""))  // TODO (Alex): Change to '!= null' if Contact changes behavior
+                email.sendMessage(c, location, null);  // Change customMessage parameter to appropriate in SharedPreferences
+            if (!c.getPhoneNumber().equals(""))  // TODO (Alex): Change to '!= null' if Contact changes behavior
+                sms.sendMessage(c, location, null);
+        }
     }
 }
