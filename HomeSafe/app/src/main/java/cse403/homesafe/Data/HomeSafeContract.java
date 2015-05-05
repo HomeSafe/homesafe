f948e3935c262755c6ba38a1b5279b39c9bb2aa2
leaf94 <leaf94@uw.edu>
2015-05-05 14:58:35 -0700

    Add Contract class

---
+++ HomeSafe/app/src/main/java/cse403/homesafe/Data/HomeSafeContract.java
@@ -0,0 +1,10 @@
+package cse403.homesafe.Data;
+
+/**
+ * Created by yellowleaf on 5/5/15.
+ * This class is a container for constants that define
+ * names for tables, and columns. Each inner class is definition of a table
+ */
+public final class HomeSafeContract {
+
+}
