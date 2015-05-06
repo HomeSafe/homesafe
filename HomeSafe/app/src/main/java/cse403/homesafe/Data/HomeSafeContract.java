4072c43548ec82388bc3fcfba7195ce55e08eda4
leaf94 <leaf94@uw.edu>
2015-05-05 17:20:48 -0700

    Define Database Schema and add database helper

--- HomeSafe/app/src/main/java/cse403/homesafe/Data/HomeSafeContract.java
+++ HomeSafe/app/src/main/java/cse403/homesafe/Data/HomeSafeContract.java
@@ -1,10 +1,35 @@
 package cse403.homesafe.Data;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by yellowleaf on 5/5/15.
  * This class is a container for constants that define
  * names for tables, and columns. Each inner class is definition of a table
  */
 public final class HomeSafeContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    public HomeSafeContract() {}
 
+    /* Inner class that defines the table 'Contact' */
+    public static abstract class ContactEntry implements BaseColumns {
+        public static final String TABLE_NAME = "Contact";
+        public static final String COLUMN_CID = "cid";
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_TIER = "tier";
+        public static final String COLUMN_EMAIL = "email";
+        public static final String COLUMN_PHONE = "phone";
+
+    }
+
+    /* Inner class that defines the table 'Location' */
+    public static abstract class LocationEntry implements BaseColumns {
+        public static final String TABLE_NAME = "Location";
+        public static final String COLUMN_LID = "lid";
+        public static final String COLUMN_NAME = "name";
+        public static final String COLUMN_LAT = "latitude";
+        public static final String COLUMN_LNG = "longitude";
+
+    }
 }
---
+++ HomeSafe/app/src/main/java/cse403/homesafe/Data/HomeSafeDbHelper.java
@@ -0,0 +1,52 @@
+package cse403.homesafe.Data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by yellowleaf on 5/5/15.
+ */
+public class HomeSafeDbHelper extends SQLiteOpenHelper {
+    // If you change the database schema, you must increment the database version.
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "HomeSafe.db";
+
+
+    // SQL query
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String INT_TYPE = " INTEGER";
+    private static final String COMMA_SEP = ",";
+    // CREATE TABLE
+    private static final String SQL_CREATE_TABLE_CONTACT = "CREATE TABLE " + HomeSafeContract.ContactEntry.TABLE_NAME + " ("
+                                                        + HomeSafeContract.ContactEntry._ID + " INTEGER PRIMARY KEY,"
+                                                        + HomeSafeContract.ContactEntry.COLUMN_CID + INT_TYPE + COMMA_SEP
+                                                        + HomeSafeContract.ContactEntry.COLUMN_TIER + INT_TYPE + COMMA_SEP
+                                                        + HomeSafeContract.ContactEntry.COLUMN_NAME + TEXT_TYPE + COMMA_SEP
+                                                        + HomeSafeContract.ContactEntry.COLUMN_EMAIL + TEXT_TYPE + COMMA_SEP
+                                                        + HomeSafeContract.ContactEntry.COLUMN_PHONE + TEXT_TYPE + " )";
+
+    private static final String SQL_CREATE_TABLE_LOCATION = "CREATE TABLE " + HomeSafeContract.LocationEntry.TABLE_NAME + " ("
+                                                        + HomeSafeContract.LocationEntry._ID + " INTEGER PRIMARY KEY,"
+                                                        + HomeSafeContract.LocationEntry.COLUMN_LID + INT_TYPE + COMMA_SEP
+                                                        + HomeSafeContract.LocationEntry.COLUMN_NAME + TEXT_TYPE + COMMA_SEP
+                                                        + HomeSafeContract.LocationEntry.COLUMN_LAT + TEXT_TYPE + COMMA_SEP
+                                                        + HomeSafeContract.LocationEntry.COLUMN_LNG + TEXT_TYPE + " )";
+
+
+    public HomeSafeDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_TABLE_CONTACT);
+        db.execSQL(SQL_CREATE_TABLE_LOCATION);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
