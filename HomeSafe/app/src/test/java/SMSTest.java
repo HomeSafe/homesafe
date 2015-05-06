e37c1d112654d340e92dc25a30085a82eb06bdbe
dliuxy94 <liuxianyang94@hotmail.com>
2015-05-05 18:35:21 -0700

    Created SMSTest.java

---
+++ HomeSafe/app/src/test/java/SMSTest.java
@@ -0,0 +1,20 @@
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by dliuxy94 on 5/5/15.
+ */
+public class SMSTest extends TestCase {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void testSendTextMessage() {
+        // assertEquals();
+    }
+}
