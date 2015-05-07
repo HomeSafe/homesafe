331599f5bc357ec5b37933a40e697e1b5c9381ae
dliuxy94 <liuxianyang94@hotmail.com>
2015-05-07 16:20:12 -0700

    SMSTest

--- HomeSafe/app/src/test/java/SMSTest.java
+++ HomeSafe/app/src/test/java/SMSTest.java
@@ -3,18 +3,26 @@ import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 
+import cse403.homesafe.Data.Contact;
+import cse403.homesafe.Messaging.SMS;
+
 /**
  * Created by dliuxy94 on 5/5/15.
  */
 public class SMSTest extends TestCase {
 
+    private SMS sms;
+    private Contact recipient;
+//    private Location location;
+    private String customMessage;
+
     @Before
     public void setUp() {
-
+        sms = SMS.getInstance();
     }
 
     @Test
     public void testSendTextMessage() {
-        // assertEquals();
+//        sms.sendMessage(recipient, location, customMessage);
     }
 }
