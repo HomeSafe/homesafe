6512583a5e755c8d1f7138fc93f8650a45c81f48
dliuxy94 <liuxianyang94@hotmail.com>
2015-05-12 18:37:10 -0700

    Added Tests

--- HomeSafe/app/src/test/java/HSTimerTest.java
+++ HomeSafe/app/src/test/java/HSTimerTest.java
@@ -3,6 +3,7 @@ import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 
+import cse403.homesafe.HSTimer;
 import cse403.homesafe.Trip;
 
 /**
@@ -10,12 +11,15 @@ import cse403.homesafe.Trip;
  */
 public class HSTimerTest extends TestCase {
 
+    private HSTimer hsTimer;
     private long estimatedTimeArrival;
     private Trip currentTrip;
 
     @Before
     public void setUp() {
-
+        estimatedTimeArrival = 60000; // in milliseconds
+        currentTrip = new Trip();
+        hsTimer = new HSTimer(estimatedTimeArrival, currentTrip);
     }
 
     @Test
