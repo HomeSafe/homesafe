74160c617114808f1ddf2f03941b745b469b6ab1
Alex Hernandez <jahrndez@uw.edu>
2015-05-12 17:19:21 -0700

    Refactored callback interface, finishing up implementation of GoogleMapsUtils.addressToLocation()

---
+++ HomeSafe/app/src/main/java/cse403/homesafe/Util/GoogleMapsUtilsCallback.java
@@ -0,0 +1,10 @@
+package cse403.homesafe.Util;
+
+/**
+ * Created by Vivek on 5/10/15.
+ * Interface for receiving callbacks from GoogleMapsUtils when their network
+ * connection results finish.
+ */
+public interface GoogleMapsUtilsCallback {
+    void callback(Object obj);
+}
