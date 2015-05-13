1856e24b706ee14c0aa5e15a95aa5e09c5b1a517
dliuxy94 <liuxianyang94@hotmail.com>
2015-05-12 18:23:13 -0700

    Added Tests

---
+++ HomeSafe/app/src/test/java/HSTimerTest.java
@@ -0,0 +1,25 @@
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import cse403.homesafe.Trip;
+
+/**
+ * Created by dliuxy94 on 5/12/15.
+ */
+public class HSTimerTest extends TestCase {
+
+    private long estimatedTimeArrival;
+    private Trip currentTrip;
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void someTest() {
+        assertEquals(false, true);
+    }
+}
---
+++ HomeSafe/app/src/test/java/TripTest.java
@@ -0,0 +1,20 @@
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by dliuxy94 on 5/12/15.
+ */
+public class TripTest extends TestCase {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Test
+    public void someTest() {
+
+    }
+}
