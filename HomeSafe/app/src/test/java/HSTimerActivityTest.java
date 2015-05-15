ebe4befa731a8f98717f08f6ef46ff1a09c941da
dliuxy94 <liuxianyang94@hotmail.com>
2015-05-14 22:36:47 -0700

    Updated HSTimerActivity, implemented HSTimerActivityTest

--- HomeSafe/app/src/test/java/HSTimerActivityTest.java
+++ HomeSafe/app/src/test/java/HSTimerActivityTest.java
@@ -9,6 +9,8 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 import cse403.homesafe.HSTimerActivity;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by dliuxy94 on 5/12/15.
  */
@@ -33,14 +35,62 @@ public class HSTimerActivityTest {
     @Test
     public void testInitProgressBar() throws Exception {
         timerActivity = PowerMockito.spy(new HSTimerActivity());
-        PowerMockito.doNothing().when(timerActivity); //.initProgressBar(); // "initProgressBar", new Object[0]);
-        PowerMockito.verifyPrivate(timerActivity).invoke("initProgressBar", new Object[0]);
+        PowerMockito.when(timerActivity, "initProgressBar").thenReturn(true);
     }
 
     @Test
     public void testCreateTimer() throws Exception {
         timerActivity = PowerMockito.spy(new HSTimerActivity());
-        PowerMockito.doNothing().when(timerActivity, "createTimer", new Object[0]);
-        PowerMockito.verifyPrivate(timerActivity).invoke("createTimer", new Object[0]);
+        PowerMockito.when(timerActivity, "createTimer", new Object[0]).thenReturn(true);
+    }
+
+    @Test
+    public void testAddToTimer() throws Exception {
+        timerActivity = PowerMockito.spy(new HSTimerActivity());
+        PowerMockito.when(timerActivity, "addToTimer", new Object[0]).thenReturn(true);
+    }
+
+    @Test
+    public void testEndTimer() throws Exception {
+        timerActivity = PowerMockito.spy(new HSTimerActivity());
+        PowerMockito.when(timerActivity, "endTimer", new Object[0]).thenReturn(true);
+    }
+
+    @Test
+    public void testParseTimeStringSec() throws Exception {
+        assertEquals(new Long(HSTimerActivity.parseTimeString("1000 sec")), new Long(1000000));
+    }
+
+    @Test
+    public void testParseTimeStringMin() throws Exception {
+        assertEquals(new Long(HSTimerActivity.parseTimeString("10 min")), new Long(600000));
+    }
+
+    @Test
+    public void testParseTimeStringHr() throws Exception {
+        assertEquals(new Long(HSTimerActivity.parseTimeString("2 hr")), new Long(7200000));
+    }
+
+    @Test
+    public void testParseTimeStringError() throws Exception {
+        assertEquals(new Long(HSTimerActivity.parseTimeString("error")), new Long(-1));
+    }
+
+    @Test
+    public void testAddItemsToTimeOptions() throws Exception {
+        timerActivity = PowerMockito.spy(new HSTimerActivity());
+        PowerMockito.when(timerActivity, "addItemsToTimeOptions", new Object[0]).thenReturn(true);
+    }
+
+    @Test
+    public void testPromptForPassword() throws Exception {
+        timerActivity = PowerMockito.spy(new HSTimerActivity());
+        PowerMockito.when(timerActivity, "promptForPassword", new Object[0]).thenReturn(true);
+    }
+
+    @Test
+    public void testBuildGoogleApiClient() throws Exception {
+        timerActivity = PowerMockito.spy(new HSTimerActivity());
+        PowerMockito.when(timerActivity, "buildGoogleApiClient", new Object[0]).thenReturn(true);
     }
 }
