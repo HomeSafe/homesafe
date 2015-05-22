c9bb7c9c17f0bad63853f752558e72f0db3f0b47
Vivek Paramasivam <paramv@cs.washington.edu>
2015-05-22 14:19:52 -0700

    StartScreenActivitySystemTest now runs four tests, with a full app teardown in between.

--- HomeSafe/app/src/androidTest/java/cse403/homesafe/test/StartScreenActivitySystemTest.java
+++ HomeSafe/app/src/androidTest/java/cse403/homesafe/test/StartScreenActivitySystemTest.java
@@ -33,7 +33,9 @@ public class StartScreenActivitySystemTest extends ActivityInstrumentationTestCa
         super(StartScreenActivity.class);
     }
 
-    @Override
+    public static final String PASSWORD = "1111";
+    public static final String SPECIAL_PASSWORD = "2222";
+
     protected void setUp() throws Exception {
         super.setUp();
         solo = new Solo(getInstrumentation(), getActivity());
@@ -45,11 +47,112 @@ public class StartScreenActivitySystemTest extends ActivityInstrumentationTestCa
         prefs.edit().putString("cse403.homesafe.app.lastName", "Lu").apply();
     }
 
+    @Override
+    public void tearDown() throws Exception {
+        solo.finishOpenedActivities();
+        super.tearDown();
+    }
+
     public void testStartTripSettingActivity() throws Exception {
         solo.clickOnButton("Start Trip");
         solo.pressSpinnerItem(0, 1);
-        solo.clickOnButton("Start Trip");
-        solo.clickOnButton("End Trip");
+        Object lock = new Object();
 
+        // wait for request to finish
+        synchronized (lock) {
+            lock.wait(500);
+        }
+
+        // on emulator, this bypasses the problem where location tracking doesnt work,
+        // so we can't estimate time.
+        solo.setTimePicker(0, 0, 1);
+        solo.clickOnButton("Start Trip");
+
+        // wait to switch screens
+        synchronized (lock) {
+            lock.wait(500);
+        }
+        solo.clickOnButton("End Trip");
+    }
+
+    public void testExtendTimeActivity() throws Exception {
+        solo.clickOnButton("Start Trip");
+        solo.pressSpinnerItem(0, 1);
+        Object lock = new Object();
+
+        // wait for request to finish
+        synchronized (lock) {
+            lock.wait(500);
+        }
+
+        // on emulator, this bypasses the problem where location tracking doesnt work,
+        // so we can't estimate time.
+        solo.setTimePicker(0, 0, 1);
+        solo.clickOnButton("Start Trip");
+
+        // wait to switch screens
+        synchronized (lock) {
+            lock.wait(500);
+        }
+        solo.pressSpinnerItem(0, 2);
+        solo.clickOnButton("Add More Time");
+
+
+        // wait to switch screens
+        synchronized (lock) {
+            lock.wait(500);
+        }
+        solo.typeText(0, PASSWORD);
+        solo.clickOnButton(0);
+        // success
+    }
+
+    public void testSecretPasswordEndTrip() throws Exception {
+        solo.clickOnButton("Start Trip");
+        solo.pressSpinnerItem(0, 1);
+        Object lock = new Object();
+
+        // wait for request to finish
+        synchronized (lock) {
+            lock.wait(500);
+        }
+
+        // on emulator, this bypasses the problem where location tracking doesnt work,
+        // so we can't estimate time.
+        solo.setTimePicker(0, 0, 1);
+        solo.clickOnButton("Start Trip");
+
+        // wait to switch screens
+        synchronized (lock) {
+            lock.wait(500);
+        }
+        solo.clickOnButton("End Trip");
+        // wait to switch screens
+        synchronized (lock) {
+            lock.wait(500);
+        }
+        solo.typeText(0, SPECIAL_PASSWORD);
+        solo.clickOnButton(0);
+        // success
+    }
+
+    public void testAddContact() throws Exception {
+        solo.clickOnActionBarHomeButton();
+        solo.clickOnMenuItem("Emergency Contacts");
+        Object lock = new Object();
+
+        // wait for request to finish
+        synchronized (lock) {
+            lock.wait(500);
+        }
+
+        solo.clickOnView(solo.getView(R.id.contacts_fab)); // floatingActionButton
+        synchronized (lock) {
+            lock.wait(500);
+        }
+        solo.typeText(0, "testName");
+        solo.typeText(1, "9999999999");
+        solo.typeText(2, "test@example.com");
+        solo.clickOnButton("Save");
     }
 }
